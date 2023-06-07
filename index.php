0c51788d024caff0b68e9a9b98f0e5c7b0b24385
mamazavr <116800609+mamazavr@users.noreply.github.com>
2023-06-07 21:14:49 +0300

    Fist commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>веб сайт</title>
+</head>
+<body>
+<div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94" role="img"></path></svg>
+        <span class="fs-4">бангладеш</span>
+    </a>
+
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <p> <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="index.php">Главная</a>
+       <p> </p> <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="contacts.html">Контакты</a>
+       <p> <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="PaymentAndDelivery.html">доставка и оплата</a>
+        <p> <a class="py-2 link-body-emphasis text-decoration-none" href="catalog.html">Католог</a>
+        <p> <a class="py-2 link-body-emphasis text-decoration-none" href="company.html">о компании</a>
+    </nav>
+</div>
+</body>
+</html>
